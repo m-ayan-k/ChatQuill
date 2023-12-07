dac026ea6685e615e441b0cbebf17989c032d9f5
m-ayan-k <mycoolfriends098@gmail.com>
2023-12-07 13:05:34 +0530

    initial commit

---
+++ app/(site)/page.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import AuthForm from "./components/AuthForm";
+
+const Auth = () => {
+  return (
+    <div 
+      className="
+        flex 
+        min-h-full 
+        flex-col 
+        justify-center 
+        py-12 
+        sm:px-6 
+        lg:px-8 
+        bg-gray-100
+      "
+    >
+    {/*<a href="https://www.flaticon.com/free-icons/dating-app-android" title="dating app android icons">Dating app android icons created by Creartive - Flaticon</a>*/}
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <Image
+          height="48"
+          width="48"
+          className="mx-auto w-auto"
+          src="/images/meetme.png"
+          alt="Logo"
+        />
+        <h2 
+          className="
+            mt-6 
+            text-center 
+            text-3xl 
+            font-bold 
+            tracking-tight 
+            text-gray-900
+          "
+          >
+            Sign in to your account
+        </h2>
+      </div>
+      <AuthForm />      
+  </div>
+  )
+}
+
+export default Auth;
